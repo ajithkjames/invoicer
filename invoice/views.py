e577d879d295c84f47cf66145b3e4bcdb3116256
ajith <ajith.kjames3@gmail.com>
2017-03-31 14:59:37 +0530

    pdf generate from server and and attach to sending mail

--- invoice/views.py
+++ invoice/views.py
@@ -1,28 +1,9 @@
-import os
-from django.conf import settings
-from django.shortcuts import render
-import easy_pdf
-from rest_framework.views import APIView
-from django.views.generic import View
-from easy_pdf.views import PDFTemplateView
-from rest_framework import views
-from rest_framework.response import Response
-from reportlab.pdfgen import canvas
 from django.http import HttpResponse
-from cStringIO import StringIO
-
-from rest_framework import status
-from rest_framework.views import APIView
-
-from drf_pdf.renderer import PDFRenderer
-from drf_pdf.response import PDFResponse
-
-from reportlab.pdfgen import canvas
 from django.core.mail import EmailMessage
-from django.core.mail import send_mail, BadHeaderError
-from rest_framework.parsers import FileUploadParser
-
-
+from django.template import loader, Context, RequestContext
+from rest_framework.views import APIView
+import easy_pdf
+from easy_pdf.views import PDFTemplateView
 
 
 class PDFView(APIView):
@@ -59,10 +40,30 @@ class send(APIView):
 		to=request.data.get('to', None)
 		sender =request.data.get('from', None)
 		message =request.data.get('message', None)
-		file = request.FILES['0']
-		filestr = file.read()
-		file_type = str(request.FILES['0'].content_type)
-		file_name = str(request.FILES['0'].name)
+		context={
+    	'number':request.data.get('number', None),
+    	'from_text':request.data.get('from_text', None),
+    	'to_text':request.data.get('to_text', None),
+    	'from':request.data.get('company'),
+    	'to':request.data.get('customer'),
+    	'logo': request.data.get('logo', None),
+    	'items': request.data.get('items', None),
+    	'subtotal':request.data.get('subtotal', None),
+    	'taxtitle':request.data.get('taxtitle'),
+    	'tax':request.data.get('tax'),
+    	'taxamount':request.data.get('taxamount'),
+    	'taxtitle1':request.data.get('taxtitle1'),
+    	'tax1':request.data.get('tax1'),
+    	'taxamount1':request.data.get('taxamount1'),
+    	'grandtotal':request.data.get('grandtotal'),
+    	'notes':request.data.get('notes'),
+    	'terms':request.data.get('terms'),
+    	'currency':request.data.get('currency'),
+    	'additionaltax':request.data.get('additionaltax'),
+    	}
+		filestr1 = loader.render_to_string("pdf.html", context)
+		filestr = easy_pdf.rendering.html_to_pdf(content=filestr1, encoding=u'utf-8')
+
 		try:
 			email = EmailMessage(
 			'Invoice from '+sender,
@@ -71,7 +72,7 @@ class send(APIView):
 			[to],
 			headers={'Message-ID': 'foo'},
 			)
-			email.attach(file_name, filestr, file_type)
+			email.attach('invoice.pdf', filestr, 'application/pdf')
 			email.send()
 		except BadHeaderError:
 			return HttpResponse('Invalid header found.')
